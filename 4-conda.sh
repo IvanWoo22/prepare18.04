114140a9777f6db9559f34d29857ed3c6aefb261
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2018-10-26 10:50:29 +0800

    Update 4-conda.sh

--- 4-conda.sh
+++ 4-conda.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo '==> During this process, you will get the latest python3.' && sleep 3
+
 echo '==> Downloading Miniconda3.'
 wget -N http://mirrors.nju.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
