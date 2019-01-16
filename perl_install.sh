ae85f0616303ca6db5e2c0bb21ae2532dc9400ee
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2019-01-16 17:00:42 +0800

    Update perl_install.sh

--- perl_install.sh
+++ perl_install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cpan YAML Config::Tiny Bio::DB::Taxonomy AlignDB::Stopwatch File::HomeDir Regexp::Common WWW::Mechanize LWP::Protocol::https
+cpan YAML Config::Tiny Bio::DB::Taxonomy AlignDB::Stopwatch File::HomeDir Regexp::Common WWW::Mechanize LWP::Protocol::https File::Find::Rule
